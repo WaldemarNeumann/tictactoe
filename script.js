0e017e4ab2db194923bdaeada644b37fca34fe09
Waldemar Neumanne <waldemar_neumann@gmx.dem>
2022-09-29 14:29:55 +0200

    last stand

---
+++ script.js
@@ -0,0 +1,75 @@
+let fields = [];
+
+let currentShape = 'cross';
+
+function fillShape(id) {
+
+    if (currentShape == 'cross') {
+        currentShape = 'circel';
+    } else {
+        currentShape = 'cross';
+    }
+
+    fields[id] = currentShape;
+    console.log(fields);
+
+    draw();
+    checkForWin();
+}
+
+function draw() {
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i] == 'circel') {
+            document.getElementById('circel-' + i).classList.remove('d-none');
+        }
+
+        if (fields[i] == 'cross') {
+            document.getElementById('cross-' + i).classList.remove('d-none');
+        }
+    }
+}
+
+function checkForWin() {
+
+    let winner;
+
+    if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
+        winner = fields[0];
+    }
+
+    if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
+        winner = fields[3];
+    }
+
+    if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
+        winner = fields[6];
+    }
+
+
+    if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
+        winner = fields[0];
+    }
+
+    if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
+        winner = fields[1];
+    }
+
+    if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
+        winner = fields[2];
+    }
+
+
+
+    if (fields[0] == fields[4] && fields[4] == fields[7] && fields[0]) {
+        winner = fields[0];
+    }
+
+    if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
+        winner = fields[2];
+    }
+
+    if (winner) {
+        console.log = 'Gewonnen:', winner;
+    }
+
+}
